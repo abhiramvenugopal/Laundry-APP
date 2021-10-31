2f9f361e4034cf0394197b5cb1a22d5408e89d4e
Anish kumar ranjan <anish727739@gmail.com>
2021-10-31 21:22:27 +0530

    port config

--- appServer/index.js
+++ appServer/index.js
@@ -1,9 +1,10 @@
 const express=require("express");
 const mongoose=require("mongoose");
 const app=express();
+
 const dotenv = require('dotenv');
 dotenv.config();
 
 app.listen(process.env.PORT,()=>{
-    console.log("server started")
+    console.log("server started " +process.env.PORT)
 })
