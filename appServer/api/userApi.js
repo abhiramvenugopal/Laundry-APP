5ebd63ffc518c8cd82fcf553ed653f17bf0c115a
Anish kumar ranjan <anish727739@gmail.com>
2021-11-01 16:45:18 +0530

    created signin and register route

--- appServer/api/userApi.js
+++ appServer/api/userApi.js
@@ -1,8 +1,58 @@
 const express=require("express");
 const mongoose=require("mongoose");
 const router=express.Router();
+const userModel= require("../model/user")
+const bcrypt=require('bcrypt')
+const jwt =require("jsonwebtoken");
 
 
-
-
+router.post('/signin',(req,res)=>{
+    const {email,password}=req.body
+    if(!email || !password){
+        return res.status(422).json({error:"please add email or password"})
+    }
+    userModel.findOne({email:email})
+    .then(savedUser=>{
+        if(!savedUser){
+            return res.status(422).json({error:"please add email or password"})
+        }
+        bcrypt.compare(password,savedUser.password)
+        .then(doMatch=>{
+            if(doMatch){
+                res.json({message:"successfully sign in"})
+                const token =jwt.sign({_id:savedUser._id},process.env.JWT_SECRET)
+                res.json({token})
+            }
+            else{
+                return res.status(422).json({error:"please add email or password"})
+            }
+        })
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
+router.post("/register",async function(req,res){
+    let reqObject={...req.body}
+    try {
+        bcrypt.hash(reqObject.password, 10,async function(err, hash) {
+            if(err){
+                res.status(500).json({status:"Encryption failed"})
+            }
+            reqObject.password=hash
+            await userModel.create(reqObject)
+            res.status(200).json({
+                status:"success",
+                message:"register success"
+            })
+            
+        });
+    } catch (error) {
+        res.status(500).json({status:"failed"})
+        console.log("error",error)
+        
+    }
+    
+    
+})
 module.exports=router
--- appServer/index.js
+++ appServer/index.js
@@ -4,11 +4,13 @@ const app=express();
 const userApi=require("./api/userApi")
 const productApi=require("./api/productApi")
 const orderApi=require("./api/orderApi")
+var bodyParser = require('body-parser')
 
 const dotenv = require('dotenv');
 dotenv.config();
-
-
+mongoose.connect(process.env.DB)
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use("/api/v1/user",userApi);
 app.use("/api/v1/product",productApi);
 app.use("/api/v1/order",orderApi);
