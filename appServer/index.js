331c6d1f3aeb1fa2847cc4c72f26cd0f0c12285a
abhiramvenugopal <abhiram.venugopal007@gmail.com>
2021-11-01 10:27:20 +0530

    added 3 api files in api folder and created route for it

--- appServer/index.js
+++ appServer/index.js
@@ -1,10 +1,20 @@
 const express=require("express");
 const mongoose=require("mongoose");
 const app=express();
+const userApi=require("./api/userApi")
+const productApi=require("./api/productApi")
+const orderApi=require("./api/orderApi")
 
 const dotenv = require('dotenv');
 dotenv.config();
 
+
+app.use("/api/v1/user",userApi);
+app.use("/api/v1/product",productApi);
+app.use("/api/v1/order",orderApi);
+
+
+
 app.listen(process.env.PORT,()=>{
-    console.log("server started " +process.env.PORT)
+    console.log("server started at port : " +process.env.PORT)
 })
