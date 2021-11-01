57289b547b797b4670afad430d6f982688641215
abhiramvenugopal <abhiram.venugopal007@gmail.com>
2021-11-01 18:26:44 +0530

    created middleware

--- appServer/api/productApi.js
+++ appServer/api/productApi.js
@@ -1,8 +1,10 @@
 const express=require("express");
 const mongoose=require("mongoose");
 const router=express.Router();
+const middleware=require("../util/middleware")
 
 
+router.use("/",middleware)
 
 
 module.exports=router
