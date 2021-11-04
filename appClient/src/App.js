da4627c7890256c957e609a9fb5b0a4581246959
LVSsandeepVarma <sandeeplakham@gmail.com>
2021-11-04 18:41:25 +0530

    created products component and side,bottom navbar

--- appClient/src/App.js
+++ appClient/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router ,Switch, Route} from "react-router-dom";
 import LandingPage from "./components/Landingpage/landing-page";
+import CreatePage from "./components/CreateOrder/create-order"
 function App() {
   return (
     <div className="App">
@@ -8,7 +9,7 @@ function App() {
         <div>
           <Switch>
                   <Route exact path='/'> <LandingPage/> </Route>
-                  {/* <Route exact path='/create'>  </Route> */}
+                  <Route exact path='/create'> <CreatePage/>  </Route>
                   {/* <Route exact path='/post' component={Nav}/> */}
           </Switch>
 
