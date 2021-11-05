4cd2ca11d0b6bb65ad4a403ad1d6dd38b4252762
Anish kumar ranjan <anish727739@gmail.com>
2021-11-05 18:25:41 +0530

    Register and login page

--- appClient/src/App.js
+++ appClient/src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
+import React from 'react';
 import { BrowserRouter as Router ,Switch, Route} from "react-router-dom";
 import LandingPage from "./components/Landingpage/landing-page";
+import Nav from "./components/Nav/nav"
+import Footer from './components/Footer/footer';
+import Register from './components/Register/register';
 function App() {
   return (
     <div className="App">
       <Router>
         <div>
           <Switch>
-                  <Route exact path='/'> <LandingPage/> </Route>
-                  {/* <Route exact path='/create'>  </Route> */}
-                  {/* <Route exact path='/post' component={Nav}/> */}
+                <Route exact path='/'><Nav/><LandingPage/><Footer/></Route>
+                <Route exact path='/Register'><Nav/><Register/><Footer/></Route>
           </Switch>
-
         </div>
       </Router>
 
