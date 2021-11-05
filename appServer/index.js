be901ff5c93bf504dad0f88bb0c35738259b7eb0
abhiramvenugopal <abhiram.venugopal007@gmail.com>
2021-11-05 19:06:00 +0530

    created counter, updated summarypage, updated past-order

--- appServer/index.js
+++ appServer/index.js
@@ -4,7 +4,9 @@ const app=express();
 const userApi=require("./api/userApi")
 const productApi=require("./api/productApi")
 const orderApi=require("./api/orderApi")
+const cors=require("cors")
 var bodyParser = require('body-parser')
+app.use(cors())
 
 const dotenv = require('dotenv');
 dotenv.config();
