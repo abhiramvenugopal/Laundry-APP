f2ce05d448e06eca9b254c645faa7ac71d5a15b9
LVSsandeepVarma <sandeeplakham@gmail.com>
2021-11-07 15:41:25 +0530

    updated functionalities in create order

--- appClient/src/components/CreateOrder/create-order.js
+++ appClient/src/components/CreateOrder/create-order.js
@@ -20,10 +20,12 @@ import bluewash from "../../assets/img/blue-wash.svg"
 import blueiron from "../../assets/img/blue-iron.svg"
 import bluepack from "../../assets/img/blue-bleach.svg"
 import bluetowel from '../../assets/img/bluetowel.svg'
-import trademark from '../../assets/img/trademark.svg'
 import Summary from '../Summary/summary'
 import PastOrder from "../PastOrder/past-order";
-
+import whitemore from "../../assets/img/whitemore.svg"
+import activelist from "../../assets/img/activelist.svg"
+import activehome from "../../assets/img/activehome.svg"
+import { getToken } from "../../utils/authOperations";
 
 function CreateOrder(){   
     const icons=[shirt,tshirt,trousers,jeans,boxers,joggers,others] 
@@ -36,12 +38,18 @@ function CreateOrder(){
     const [orders, setOrders] = useState({});
     const [active, setActive] = useState("create");
     const [create, setCreate] = useState(false);
-
+    const [moreicon,setMoreicon]=useState(more)
+    const [listicon,setListicon]=useState(list)
+    const [homeIcon,setHomeicon]=useState(home)
+    
     useEffect(()=>{
-        axios({
-            url:'http://localhost:3005/api/v1/product/products',
-            method:"GET"
-        })
+        let token=getToken()
+        let header={Authorization:"bearer "+token}
+        axios.get(
+            'http://localhost:3005/api/v1/product/products',
+            {headers:header}
+
+        )
         .then(res=>{
             setTimeout(() => {
                 setProducts(Object.values(res.data.products))
@@ -54,10 +62,12 @@ function CreateOrder(){
 const handleSubmit=()=>{
     var products=[]
     var finalcost=0
+    var totalQuantity=0
     for(let i=0; i<costs.length;i++){
         var total=0
         var service=[]
         if(costs[i][0]>0 && costs[i][1]>0){
+            totalQuantity+=Number(costs[i][0])
             total+=costs[i][0]*costs[i][1]
             finalcost+=total
             if(costs[i][2]!==0){
@@ -86,9 +96,10 @@ const handleSubmit=()=>{
         "products":products,
         "subtotal":finalcost,
         "pickupCharge":90,
-        "total":finalcost+90
+        "total":finalcost+90,
+        "totalQuantity":totalQuantity
     })   
-    console.log(orders)
+    console.log(orders,totalQuantity)
     setSummary(true)
 
 }
@@ -103,14 +114,15 @@ const handlereset=(e)=>{
     setCosts(amount)
     setColor(changeColor)
     setImage(images)
-
+    document.querySelector(`[id='${e.target.name}']`).value=''
+    
 }
 const handleCancle=()=>{
     const amount=[[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0]]
     setCosts(amount)
     setColor(['black','black','black','black','black','black','black'])
     setImage([[wash,iron,towel,packing],[wash,iron,towel,packing],[wash,iron,towel,packing],[wash,iron,towel,packing],[wash,iron,towel,packing],[wash,iron,towel,packing],[wash,iron,towel,packing]])
-    
+    document.querySelectorAll('input').forEach(input=>{input.value=''})
 }
 
 const handleWash=(e)=>{
@@ -134,10 +146,13 @@ const handleWash=(e)=>{
         setImage(images)
     }
     else{
-        changeColor[e.target.name]='black'
+        if(arr[e.target.name][1]===0){
+            changeColor[e.target.name]='black'
+            setColor(changeColor)     
+        }
         images[e.target.name][0]=wash
         setImage(images)
-        setColor(changeColor) 
+        
     }
 }
 const handleFold=(e)=>{
@@ -161,8 +176,10 @@ const handleFold=(e)=>{
         setColor(changeColor)
     }
     else{
-        changeColor[e.target.name]='black'
-        setColor(changeColor) 
+        if(arr[e.target.name][1]===0){
+            changeColor[e.target.name]='black'
+            setColor(changeColor)     
+        }
         images[e.target.name][1]=iron
         setImage(images)
     }
@@ -189,8 +206,10 @@ const handlePress=(e)=>{
         setColor(changeColor)
     }
     else{
-        changeColor[e.target.name]='black'
-        setColor(changeColor) 
+        if(arr[e.target.name][1]===0){
+            changeColor[e.target.name]='black'
+            setColor(changeColor)     
+        }
         images[e.target.name][2]=towel
         setImage(images)
     }
@@ -217,8 +236,10 @@ const handdlePack=(e)=>{
         setImage(images)
     }
     else{
-        changeColor[e.target.name]='black'
-        setColor(changeColor)
+        if(arr[e.target.name][1]===0){
+            changeColor[e.target.name]='black'
+            setColor(changeColor)     
+        }
         images[e.target.name][3]=packing
         setImage(images) 
     }
@@ -238,12 +259,13 @@ const handleQuantity=(e)=>{
     }
     
 }
+
     return(
-        <div>
+        <div className="orderscomponent">
             <div className="sidebar">
-                <a href="/"><img src={home}alt="home"></img></a>
-                <a onClick={()=>{setActive("create")}} className={(active==="create")? "active": " "}><img src={more}alt="home"></img></a>
-                <a onClick={()=>{setActive("past")}} className={(active==="past")? "active": " "} ><img src={list}alt="home"></img></a>
+                <a  onClick={()=>{setListicon(list);setMoreicon(whitemore);setHomeicon(activehome)}} href="/"><img className="sidebaricons"  src={homeIcon}alt="home" name="0"></img></a>
+                <a name="1"  onClick={()=>{setActive("create");setListicon(list);setMoreicon(more);setHomeicon(home)}} className={(active==="create")? "active": " "}><img className="sidebaricons" src={moreicon}alt="home"></img></a>
+                <a name="2" onClick={()=>{setActive("past");setListicon(activelist);setMoreicon(whitemore);;setHomeicon(home)}}  className={(active==="past")? "active": " "} ><img className="sidebaricons" src={listicon}alt="home"  ></img></a>
             </div>
             { active==="create" &&
             <div>
@@ -261,52 +283,51 @@ const handleQuantity=(e)=>{
                     <table className="table table-hover container-table">
                         <thead className="table-dark thead table-head">
                             <tr>
-                                <th scope="col">Product Types</th>
-                                <th scope="col">Quantity</th>
-                                <th scope="col">WashType</th>
-                                <th scope="col">Price</th>
-                                <th scope="col"></th>
+                                <th className="col-md-3  headings" scope="col">Product Types</th>
+                                <th className="col-md-3 headings" scope="col">Quantity</th>
+                                <th className="col-md-4 headings" scope="col">WashType</th>
+                                <th className="col-md-1 headings" scope="col">Price</th>
+                                <th className="col-md-1 headings" scope="col" style={{"color":"transparent"}}> reset</th>
                             </tr>
                         </thead>
                         <tbody  className="table-body">
                             {Object.values(products).map((each,ind)=>{
                                 return(            
-                                    <tr>
-                                        <th className="trow"  scope="row">
-                                            <td className="productname">
+                                    <tr key={ind}>
+                                            <td scope="row" className="productname">
                                                 <div>
                                                     <img src={icons[ind]} alt="img"  width="45" height="45" ></img>
                                                 </div>
                                                 <div className="productdetails"  >
-                                                    <p style={{"color":color[ind]}}>{each.productType}</p>
-                                                    <p  style={{"fontSize":"10px"}}>{each.description}</p>
+                                                    <p style={{"color":color[ind],"fontSize":"100%"}}>{each.productType}</p>
+                                                    <p  style={{"fontSize":"80%","color":"#76788B"}}>{each.description}</p>
                                                 </div>
                                             </td>
-                                        </th>
-                                        <th scope="row"><td><input onChange={handleQuantity} className="Quantity" type="number"  min="0" name={ind}></input></td></th>
-                                        <th scope="row">
+                                        
+                                        <td scope="row"><input id={ind} onChange={handleQuantity} className="Quantity" type="number"  min="1" name={ind}></input></td>
+                                        
                                         <td><div className="imagescolumn">
-                                            <img onClick={handleWash} style={{"color":color[ind]}}   src={image[ind][0]} alt="img" name={ind}></img>
-                                            <img onClick={handleFold}   className="img" src={image[ind][1]} alt="img"   name={ind} ></img>
-                                            <img onClick={handlePress}  className="img" src={image[ind][2]} alt="img"  name={ind} ></img>
-                                            <img onClick={handdlePack}  className="img" src={image[ind][3]} alt="img"  name={ind}></img>
+                                            <img onClick={handleWash}  style={{"color":color[ind],}}   src={image[ind][0]}   alt="img" name={ind}></img>
+                                            <img onClick={handleFold} style={{"paddingLeft":"4.5vw"}}  className="img" src={image[ind][1]} alt="img"   name={ind} ></img>
+                                            <img onClick={handlePress} style={{"paddingLeft":"4.5vw"}} className="img" src={image[ind][2]} alt="img"  name={ind} ></img>
+                                            <img onClick={handdlePack} style={{"paddingLeft":"4.5vw"}} className="img" src={image[ind][3]} alt="img"  name={ind}></img>
                                         </div>
                                         </td>
-                                        </th>
+                                        
                                             
-                                            <th scope="row"><td className={(costs[ind][1]>0?"display":"hide")} >{costs[ind][0]}x{costs[ind][1]}=<b style={{color:'#5861AE'}}>{costs[ind][0]*costs[ind][1]}</b></td>
-                                            <td className={(costs[ind][1]>0?"hide":"display")}>--</td></th>
-                                        <th scope="row">
-                                            <button name={ind} onClick={handlereset} type="submit" className={(costs[ind][1]>0 ? " btn btn-outline-primary button":"hide" )}>reset</button>
-                                            <td></td>
-                                        </th>
+                                            <td scope="row" className={(costs[ind][1]>0?"display":"hide")} >{costs[ind][0]}x{costs[ind][1]}=<b style={{color:'#5861AE'}}>{costs[ind][0]*costs[ind][1]}</b></td>
+                                            <td className={(costs[ind][1]>0?"hide":"display")}>--</td>
+                                        
+                                            <td><button name={ind} onClick={handlereset} type="submit" className={(costs[ind][1]>0 ? " btn btn-outline-primary button reset":"hide" )}>reset</button></td>
+                                            
+                                        
                                     </tr>
                                 )
                             })}
                             </tbody>
                             
                     </table>  
-                    <div className={((costs[0][0]) && costs[0][1]) || (costs[1][0] && costs[1][1])||(costs[2][0] && costs[2][1])||(costs[3][0] && costs[3][1])||(costs[4][0] && costs[4][1])|| (costs[5][0] && costs[5][1])?"buttons":"hidebuttons" }>
+                    <div className={(costs[0][0] && costs[0][1]) || (costs[1][0] && costs[1][1])||(costs[2][0] && costs[2][1])||(costs[3][0] && costs[3][1])||(costs[4][0] && costs[4][1]) || (costs[5][0] && costs[5][1]) || (costs[6][0] && costs[6][1])?"buttons":"hidebuttons" }>
                                 <button onClick={handleCancle}  className="btn btn-outline-primary cancel button">cancel</button>
                                 <button type="button" onClick={handleSubmit} className="btn btn-primary proceed button" >proceed</button>
                             </div>
